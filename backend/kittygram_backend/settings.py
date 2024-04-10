10c5c3140caee7879a071294084ee166ba9d64c5
Kobelev Andrey <heiskobe@yandex.ru>
2024-04-10 16:59:51 +0300

    Show the project for the 2nd review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'error-secret-key')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG_VALUE') == 'True'
 
 
 ENVS = (
@@ -64,18 +64,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
-    }
+POSTGRESQL = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('POSTGRES_DB', 'django'),
+    'USER': os.getenv('POSTGRES_USER', 'django'),
+    'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+    'HOST': os.getenv('DB_HOST', ''),
+    'PORT': os.getenv('DB_PORT', 5432)
 }
 
+SQLITE = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+}
+
+DATABASES = dict()
+
+if not os.getenv('SQLITE', False):
+    DATABASES['default'] = POSTGRESQL
+else:
+    DATABASES['default'] = SQLITE
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
