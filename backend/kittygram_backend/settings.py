5c44d8b974335169ed499a88f774a8d4b4240be8
Kobelev Andrey <heiskobe@yandex.ru>
2024-04-11 10:53:57 +0300

    Show the project for the 3rd review (fix allowed hosts)

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'error-secret-key')
 
 DEBUG = os.getenv('DEBUG_VALUE') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
